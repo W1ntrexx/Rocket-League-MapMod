381b949aef77524c117b02ab5edb4bf15b63e25a
Wlad <wdukvin@duck.com>
2026-05-30 00:30:22 +0200

    Band Aid 2 for tweaks rq.

--- menus.py
+++ menus.py
@@ -3,10 +3,6 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import messagebox
 
-
-mapChangerFrame = None
-skinChangerFrame = None
-
 def showTweaks(window):
     global tweaksFrame, mapChangerFrame, skinChangerFrame
     
@@ -18,26 +14,5 @@ def showTweaks(window):
     )
     tweaksFrame.place(x=200, y=0)
 
-    mapChangerFrame = ctk.CTkFrame(
-        window,
-        width=400, height=400,
-        fg_color="#2b2b2b",
-        corner_radius=0,
-    )
-    mapChangerFrame.place(x=200, y=0)
-
-    skinChangerFrame = ctk.CTkFrame(
-        window,
-        width=400, height=400,
-        fg_color="#2b2b2b",
-        corner_radius=0,
-    )
-    skinChangerFrame.place(x=200, y=0)
 
 
-
-def showMapChanger(e):
-    mapChangerFrame.lift()
-
-def showSkinChanger(e):
-    skinChangerFrame.lift()
